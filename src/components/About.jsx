ac937277386ffecfdf01e95ad2f52f49442addaa
ramonsabino <ramon-sabino@hotmail.com>
2023-03-28 15:39:55 -0300

    fix: correcao about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,16 +7,17 @@ const About = () => {
       <p>
         Olá me chamo Ramon Sabino, estudante e apaixonado por tecnologia, este
         portifolio é uma breve apresentação do meu trabalho e um pouco do meu
-        conhecimento. Agora irei contar um pouco de como eu comecei neste mundo!
-        Começou quando eu tinha por volta dos 7 a 8 anos de idade e meu irmão
-        que é mais velho que na época era técnico de informática, ele havia
-        levado um computador para casa de cara já gostei, logo comecei a fazer
-        reparos básicos em computadores e vi que nasci para fazer isso. No meu
-        crescimento pude conhecer vários outros ramos da informática como o
-        Desenvolvimento. Hoje Estudo as linguagens do ecosistema JavaScript
-        (React e Node) no qual tive experiência profissional Bom este é apenas
-        uma breve apresentação e resumo sobre mim, qualquer coisa estou
-        disponível nas redes sociais abaixo!
+        conhecimento. Irei contar um pouco de como eu comecei neste mundo!
+        Quando eu tinha entre 7 e 8 anos de idade tive meu primeiro contato com
+        um problema de um computador, irmão mais velho que na época era técnico
+        de informática, levou um computador para casa e de cara já gostei! Logo
+        comecei a fazer reparos básicos em computadores e vi que nasci para
+        fazer isso. No meu crescimento pude conhecer vários outros ramos da área
+        de T.I como o de Redes de Computadores, Segurança Cibernética e
+        Desenvolvimento de Softwares. Hoje Estudo as linguagens do ecossistema
+        JavaScript (React e Node) no qual tive experiência profissional. Bom
+        esta é apenas uma breve apresentação e resumo sobre mim, qualquer coisa
+        estou disponível nas redes sociais abaixo!
       </p>
     </div>
   );
